6a4b860b08ad427790444398774b643d13caad52
Agustina <agustinaayelensosa@gmail.com>
2024-07-24 08:51:16 -0300

    ejercicios 1 y 2

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Countdown from './components/Contador';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const targetDate = new Date('2024-09-28T23:59:59');
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Cuenta regresiva</h1>
+      <Countdown targetDate={targetDate} />
+    </div>
+  );
 }
+
 
 export default App
